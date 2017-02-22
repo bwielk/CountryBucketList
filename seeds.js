dd96c0b904b23a62db193d17eb6ca48bd24bdf36
Blazej <bwielk@gmail.com>
2017-02-22 15:19:33 +0000

    Create database

---
+++ seeds.js
@@ -0,0 +1,16 @@
+use countries;
+
+db.dropDatabase();
+
+db.bucketList.insert([
+{
+  country: "Republic of Macedonia",
+  capital: "Skopje",
+  population: "2058539",
+  latlng: [ 41833333, 22
+  ]
+}
+
+  ]);
+
+db.bucketList.find();
